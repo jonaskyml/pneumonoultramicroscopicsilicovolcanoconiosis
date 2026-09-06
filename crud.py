5df436123d76af0892db05e7da59dbfb100a2430
jønas <jonaskyml@proton.me>
2026-09-06 18:52:11 +0200

    condition for checkcing if the user wants to scan a built-in dictionary or their local file

--- crud.py
+++ crud.py
@@ -8,29 +8,43 @@ import click
 @click.argument('filename')
 # @click.option("--file", prompt='File name', help='Scan file for the longest word.')
 
+
+
 def scan(filename: str):
     """Scan dictionary for the longest word."""
 
-    with open(filename, 'r', encoding="utf-8") as file:
-        i = 0
-        word_longest = ""
+    dictionaries = ["en", "cz"]
 
-        for line in file:
-            if not line.strip():
-                continue
+    if Path(filename).exists(): # + is it contained in dictionares array
+        sourcefile = click.prompt(
+            f"\nWhich of the following do you wish to scan?\n\n    [0] Built-in dictionary ({filename})\n    [1] Local file ({filename})\n",
+            type=click.Choice(["0", "1"]),
+            show_choices=False
+        )
 
-            length_current = len(line.strip())
+    if sourcefile == "1":
+        with open(filename, 'r', encoding="utf-8") as file:
+            i = 0
+            word_longest = ""
 
-            if length_current > i:
-                word_longest = line.strip()
-                length_longest = length_current
-                i = length_current
+            for line in file:
+                if not line.strip():
+                    continue
 
-        if not word_longest.strip():
-            click.echo("The file is empty!")
-        else:
-            #  click.echo(f"word: '{word_longest}' | legnth: '{length_longest}'")
-            click.echo(word_longest)
-            click.echo(length_longest)
+                length_current = len(line.strip())
 
+                if length_current > i:
+                    word_longest = line.strip()
+                    length_longest = length_current
+                    i = length_current
+
+            if not word_longest.strip():
+                click.echo("The file is empty!")
+            else:
+                #  click.echo(f"word: '{word_longest}' | legnth: '{length_longest}'")
+                click.echo(f"\n{word_longest}")
+                click.echo(length_longest)
+    else:
+        click.echo("you chose a dictionary")
+        # code for accessing the dictionary
 
